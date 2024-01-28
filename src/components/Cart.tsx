8c080c6efd67a968ad1ecc635191504627ca1260
edanTal <edant81@gmail.com>
2024-01-28 01:15:22 +0100

    Add empty cart state

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -15,6 +15,7 @@ import {
 } from './ui/sheet'
 import { Separator } from './ui/separator'
 import { buttonVariants } from './ui/button'
+import Image from 'next/image'
 
 const Cart = () => {
   const itemCount = 0
@@ -74,7 +75,31 @@ const Cart = () => {
             </div>
           </>
         ) : (
-          <></>
+          <div className='flex h-full flex-col items-center justify-center space-y-1'>
+            <div
+              aria-hidden='true'
+              className='relative mb-4 h-60 w-60 text-muted-foreground'
+            >
+              <Image
+                src='/hippo-empty-cart.png'
+                fill
+                alt='Shopping cart empty'
+              />
+            </div>
+            <div className='text-xl font-semibold'>Your cart is empty</div>
+            <SheetTrigger asChild>
+              <Link
+                href='/products'
+                className={buttonVariants({
+                  variant: 'link',
+                  size: 'sm',
+                  className: 'text-sm text-muted-foreground',
+                })}
+              >
+                Add items to your cart to checkout
+              </Link>
+            </SheetTrigger>
+          </div>
         )}
       </SheetContent>
     </Sheet>
